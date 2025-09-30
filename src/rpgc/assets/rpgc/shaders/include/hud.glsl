738549d7ee711bea64c252f05310ae017134d750
DawnKiro <kirobyte4@gmail.com>
2025-09-30 18:21:52 -0500

    HP bar mostly working

--- src/rpgc/assets/rpgc/shaders/include/hud.glsl
+++ src/rpgc/assets/rpgc/shaders/include/hud.glsl
@@ -1,9 +1,16 @@
-ivec3 control = ivec3(floor(Color.rgb * 255.0 + 0.5));
+ivec4 control = ivec4(floor(Color * 255.0 + 0.5));
 ivec2 guiSize = ivec2(ceil(2.0 / ProjMat[0][0]), ceil(2.0 / -ProjMat[1][1]));
 
-if (control.rg == ivec2(1, 254)) {
+if (control == ivec4(0, 0, 0, 63)) pos = vec3(0.0);
+else if (control.rg == ivec2(1, 254)) {
   int type = control.b;
   vertexColor.rgb = vec3(1.0);
-  if (type == 1) pos.y += guiSize.y - 1000.0;
+  switch (type) {
+    case 0:
+      pos = vec3(0.0);
+    break;
+    case 1:
+      pos.y += guiSize.y - 1000.0;
+    break;
+  }
 }
-
